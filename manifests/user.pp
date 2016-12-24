f904e45bca72fbec2e5ae27d55f5337ac61812ec
Dhanasekaran <nndhanasekaran@gmail.com>
2016-12-24 13:48:35 +0530

    user.pp not required

--- manifests/user.pp
+++
@@ -1,9 +0,0 @@
-class splunkuf::user (
-  $splunk_home  = '/home/splunk',
-) inherits splunkuf::params {
-  user { 'splunk':
-    ensure      => 'present',
-    managehome  => true,
-    home        => "$splunk_home",
-  }
-}
